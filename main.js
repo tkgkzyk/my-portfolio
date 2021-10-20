86513a0b9a821f7b803f0e86908ed8d41fba8425
tkgkzyk <tkgkzyk1018@gmail.com>
2021-10-21 01:12:35 +0900

    portfolio

---
+++ main.js
@@ -0,0 +1,78 @@
+const favoContainer = document.getElementById("favo-container");
+const favoTitle = document.getElementById("favo-title");
+const favoText = document.getElementById("favo-text");
+const favoPhoto = document.getElementById("favo-photo");
+const favoButton = document.getElementById("favo-button");
+const favoBackButton = document.getElementById("favo-back-button");
+let count = 0;
+
+favoButton.onclick = function () {
+  if (count < 5) {
+    count++;
+    changeContent(count);
+  }
+};
+
+favoBackButton.onclick = function () {
+  if (count >= 0) {
+    count--;
+    changeContent(count);
+  }
+};
+
+const changeContent = function (num) {
+  if (num == -1) {
+    count = 3;
+    changeContent3();
+  }
+  if (num == 0) {
+    changeContent0();
+  }
+  if (num == 1) {
+    changeContent1();
+  }
+  if (num == 2) {
+    changeContent2();
+  }
+  if (num == 3) {
+    changeContent3();
+  }
+  if (num == 4) {
+    changeContent4();
+  }
+  if (num == 5) {
+    count = 0;
+    changeContent0(); //なんでこれできないのか聞く>>>document.body.innerHTML = defaultHTML;
+  }
+};
+
+const changeContent0 = function () {
+  favoTitle.innerHTML = "<p>J-Rock</p>";
+  favoText.innerHTML =
+    "<div>KANA-BOON, My Hair is Bad, Saucy Dog, etc...</div>";
+  favoPhoto.style.backgroundImage = 'url("../images/IMG_7410.jpg")';
+};
+
+const changeContent1 = function () {
+  favoTitle.innerHTML = "<p>Soccer</p>";
+  favoText.innerHTML = "<div>ElementarySchool~HighSchool</div>";
+  favoPhoto.style.backgroundImage = 'url("../images/IMG_4677.jpg")';
+};
+
+const changeContent2 = function () {
+  favoTitle.innerHTML = "<p>Dog</p>";
+  favoText.innerHTML = "<div>Pet's Name : Lea</div>";
+  favoPhoto.style.backgroundImage = 'url("../images/IMG_7302.jpg")';
+};
+
+const changeContent3 = function () {
+  favoTitle.innerHTML = "<p>Walk</p>";
+  favoText.innerHTML = "<div>With Lea</div>";
+  favoPhoto.style.backgroundImage = 'url("../images/IMG_7550.jpg")';
+};
+
+const changeContent4 = function () {
+  favoTitle.innerHTML = "<p>Unity</p>";
+  favoText.innerHTML = "<div>GameEngine, CommingSoon...</div>";
+  favoPhoto.style.backgroundImage = 'url("../images/Unity.jpeg")';
+};
